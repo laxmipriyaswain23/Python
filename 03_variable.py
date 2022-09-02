aadc045c369b5bcbee23b23d8a67ffa7d3f1f580
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-02 22:49:08 +0530

    my first commit

---
+++ 03_variable.py
@@ -0,0 +1,84 @@
+#Ex-1
+#a=60
+#print(a)
+#print(a*2)
+#print(a/4)
+
+#Ex-2
+#how to add two numbers
+#num1=10
+#num2=20
+#result=num1+num2
+#print(result)
+
+#Ex-3
+#num1=10
+#print(num1)
+
+#Ex-4
+#num1=50
+#print(num1)
+
+#EX-5
+#num1=45
+#print(num1)
+
+#EX-6
+abc=90 #valid 
+print(abc)
+
+#Ex-7
+abc190=900 #valid
+print(abc190)
+
+#Ex-8
+abc_abc=70  #valid
+print(abc_abc)
+
+#Ex-9
+#abc@123=30 #invalid
+#print(abc@123)
+
+#Ex-10
+cash=1000 #valid
+print(cash)
+
+#Ex-11
+#ca$h=60 #invalid
+#print(ca$h)
+
+#Ex-12
+abc1234=1234 #valid
+print(abc1234)
+
+#Ex-13
+#1234abc=70
+#print(1234abc)
+
+#Ex-14
+_abc=890
+print(_abc)
+
+#Ex-15 
+a=19
+print(a)
+
+#Ex-16
+A=2
+print(A)
+
+#Ex-17
+IF=20
+print(IF)
+
+#Ex-18
+#if=6 #invalid bcz keywords can't be variables
+#print(if)
+
+#Ex-19
+#True=8 #invalid bcz keywords can't be variables 
+#print(True)
+
+#Ex-20
+_xyz_abc_sss=567
+print(_xyz_abc_sss)
---
+++ 04_keywords.py
@@ -0,0 +1,16 @@
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+a=90
+print(a)
+#if=10
+#print(if)//Error bcz 'if' is a predefined word
+
+IF=10 #valid
+print(IF)
+
+If=8
+print(If)
+
+iF=7
+print(iF)
