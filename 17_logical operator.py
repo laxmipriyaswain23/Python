5bd71ac4ed6a9c02476f86a0e9a5e48f68c2c3de
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-07 22:18:57 +0530

    my fifth commit

---
+++ 17_logical operator.py
@@ -0,0 +1,40 @@
+#FOR BOOLEAN TYPE 
+#for AND
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+#For OR
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+#for NOT
+print(not True)
+print(not False)
+
+#FOR NON-BOOLEAN TYPE
+# for (x and y)
+print(10 and 15)
+print(0 and 15)
+print(13 and 0)
+print(0 and 0)
+
+#for(x or y)
+print(10 or 15)
+print(0 or 15)
+print(13 or 0)
+print(0 or 0)
+
+#for(not x)
+print(not 13)
+print(not 'laxmi')
+print(not 0)
+
+print('laxmi'and 'laxmipriya')
+print('laxmi'or 'laxmipriya')
+
+#EMPTY STRING
+print(not '')
