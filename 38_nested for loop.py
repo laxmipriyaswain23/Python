199794d7d01c9686a453ac23f3b3e5531b7124b2
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-18 23:47:38 +0530

    my eleventh commit

---
+++ 38_nested for loop.py
@@ -0,0 +1,19 @@
+#Ex-1
+for i in range(3):
+    for j in range(3):
+        print('i = {}   j = {}'.format(i,j))
+
+#Ex-2
+for i in range(3):
+    for j in range(3):
+        print('*',end=" ")
+      
+    print("")
+
+#Ex-3
+for i in range(3):
+    for j in range(3):
+        print(i,end=" ") #for row
+       # print(j,end=" ") #for column
+      
+    print("")
