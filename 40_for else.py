b203d70cfac5633344d0c4c625d6937c2b3c7afe
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-20 23:32:22 +0530

    my eleventh commit

---
+++ 40_for else.py
@@ -0,0 +1,13 @@
+#Ex-1
+# for i in range(10):
+#     print(i)
+# else:
+#     print('I am else block')    
+
+#Ex-2
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+else:
+    print('I am else block')    
---
+++ 41_delete and none .py
@@ -0,0 +1,33 @@
+#del
+#Ex-1
+a=100
+print(a)
+# del a
+# print(a) #delete a so we will get name error
+
+#Ex-2
+x='Aaru'
+print(x)
+# del s
+# print(s)
+
+#Ex-3
+x='Aaru'
+print(x)
+# del s[0]
+# print(s) #can't delete
+
+#Ex-4
+l=[10,20,30,40,50]
+print(l)
+del l[2]
+print(l) #we can delete from list
+
+#none
+#Ex-5
+a="apple"
+print(a)
+a=None
+print(a)
+a="bag"
+print(a)
