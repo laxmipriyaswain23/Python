0121fe2d4ba51273295ee416e3329655f737a768
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-28 19:55:34 +0530

    my eleventh commit

---
+++ 49_print list in reverse order.py
@@ -0,0 +1,14 @@
+# #Wap to print a list in reverse order by using positive index
+# l=[10,20,30,40,50,60,70]
+# n=len(l)-1
+# while n>=0:
+#     print(l[n])
+#     n=n-1
+
+
+#print list in reverse order by using negative index
+l=[10,20,30,40,50,60,70]
+i=-1
+while i>=-len(l):
+    print(l[i])
+    i=i-1
