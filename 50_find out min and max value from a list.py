f405f7ad04dd7d2009533252d2f275df22b4cacc
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-09-29 19:41:56 +0530

    my eleventh commit

---
+++ 50_find out min and max value from a list.py
@@ -0,0 +1,19 @@
+#Ex-1
+#For min
+l=[10,20,30,40,50,60,70,80]
+min=l[0]
+for i in l:
+    if i <min:
+        min=i
+
+print('min is = ',min)        
+
+#for max
+#EX-2
+l=[10,20,30,40,50,60,70,80]
+max= l[0]
+for i in l:
+    if i>max:
+        max=i
+
+print('max is = ',max)        
