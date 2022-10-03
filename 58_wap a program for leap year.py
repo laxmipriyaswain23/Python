8b275119503a5e2129059312e316382057239b1f
laxmipriya swain <swainlaxmipriya529@gmail.com>
2022-10-03 22:21:48 +0530

    my tweleve commit

---
+++ 58_wap a program for leap year.py
@@ -0,0 +1,9 @@
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{} is a leap year".format(year))
+
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{} is a leap year".format(year))
+else:
+    print("{} is not a leap year".format(year))
